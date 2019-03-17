d012c5b2a2fdabee933d4c2f53ecb78d174eec18
Kangze Huang <kangzeroo@gmail.com>
2019-03-16 22:47:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function loop() {
     var rand = Math.round(Math.random() * 5000);
-    const like = document.evaluate(`//*[@id="content"]/div/span/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[4]`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
+    const like = document.evaluate(`//*[@id="content"]/span/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[3]`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
     setTimeout(function() {
             like.click()
             loop();
