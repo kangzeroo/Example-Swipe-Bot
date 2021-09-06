871f4f23717c73e99c52a54cc198a55d176d4982
Kangze Huang <kangzeroo@gmail.com>
2021-09-06 11:53:58 +0800

    Updated XPath

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function loop() {
     var rand = Math.round(Math.random() * 5000);
-    const like = document.evaluate(`//*[@id="content"]/span/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[3]`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
+    const like = document.evaluate(`//*[@id="o-738591094"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div/div[5]/div/div[4]/button`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
     setTimeout(function() {
             like.click()
             loop();
