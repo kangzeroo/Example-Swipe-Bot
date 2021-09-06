b80ce5ea9df272f6c8f6f574d5456c363ac29130
Kangze Huang <kangzeroo@gmail.com>
2021-09-06 14:07:52 +0800

    max 100 swipes

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
-const buttons = document.body.getElementsByTagName("button");
-let likeButton;
-let dislikeButton;
-let shouldContinue = true
-for (var i = 0; i < buttons.length; i++) {
-  if (buttons[i].className.indexOf("c-like-green") > -1) {
-    likeButton = buttons[i];
-  }
-  if (buttons[i].className.indexOf("c-pink") > -1) {
-    dislikeButton = buttons[i];
-  }
-}
-
-setTimeout(() => {
-   shouldContinue = false
-}, 1000*60*10)
-
-(function loop() {
-  var rand = Math.round(Math.random() * 5000);
-  setTimeout(function () {
-    const randomNumber = Math.floor(Math.random() * 100) + 1;
-    if (randomNumber > 10) {
-      likeButton.click();
-    } else {
-      dislikeButton.click();
+  const buttons = document.body.getElementsByTagName("button");
+  let likeButton;
+  let dislikeButton;
+  let maxSwipes = 100
+  let swipeCount = 0
+  for (var i = 0; i < buttons.length; i++) {
+    if (buttons[i].className.indexOf("c-like-green") > -1) {
+      likeButton = buttons[i];
     }
-    if (shouldContinue) {
-      loop();
+    if (buttons[i].className.indexOf("c-pink") > -1) {
+      dislikeButton = buttons[i];
     }
-  }, rand);
-})();
+  }
+  (function loop() {
+    var rand = Math.round(Math.random() * 5000);
+    setTimeout(function () {
+      const randomNumber = Math.floor(Math.random() * 100) + 1;
+      if (randomNumber > 10) {
+        likeButton.click();
+      } else {
+        dislikeButton.click();
+      }
+      swipeCount = swipeCount+1
+      if (swipeCount < maxSwipes) {
+        loop();
+      }
+    }, rand);
+  })();
